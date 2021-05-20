75d675c093aa060023e93aff2e572ba0148582cb
Densatho <Densatho@github.com>
2021-05-20 20:48:49 -0300

    Ajustando nome coluna

--- models/anuncio.py
+++ models/anuncio.py
@@ -11,5 +11,5 @@ class Announce(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     local = db.Column(db.Integer, db.ForeignKey(Local.id), nullable=False)
-    cost = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(240), nullable=False)
